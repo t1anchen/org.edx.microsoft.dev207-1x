0a8410622478a1c47256444ec84a4a72d5d05090
au9ustine <duke.augustine@gmail.com>
2015-10-09 23:56:34 +0800

    add Introduction

---
+++ Introduction/Introduction/Assignment.fs
@@ -0,0 +1,16 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+open System
+
+let v (r: float, h: float) = 
+    2.0 * 3.14 * Math.Pow(r, 2.0) * h
+
+[<EntryPoint>]
+let main argv = 
+    let r = float (Console.ReadLine())
+    let h = float (Console.ReadLine())
+    let ret = v r h
+    printf "%f" ret
+    let dummy = Console.ReadLine()
+    0 // return an integer exit code
